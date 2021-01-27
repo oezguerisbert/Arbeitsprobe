ec993890548c09353c0ff3d331b7b88e62660809
Özgür Isbert <oezguerisbert@gmail.com>
2021-01-27 12:15:02 +0100

    fixed mysql docker image

--- setup.sh
+++
@@ -1,3 +0,0 @@
-docker-compose up --build -d
-$IP = $(docker inspect $(docker ps --quiet --filter name=arbeitsprobe-mysql) --format "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}")
-echo $IP;
