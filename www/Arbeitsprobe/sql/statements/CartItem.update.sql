22911950c5a057d4d86c84189d9e9995dced812a
Özgür Isbert <oezguerisbert@gmail.com>
2021-02-18 19:03:31 +0100

    added transactions for cartitems

--- www/Arbeitsprobe/sql/statements/CartItem.update.sql
+++ www/Arbeitsprobe/sql/statements/CartItem.update.sql
@@ -1,8 +1,10 @@
-UPDATE kxi_cartitem
-    SET `firstname` = :firstname,
-    `lastname` = :lastname,
-    `serviceid` = :serviceid,
-    `gender` = :gender,
-    `height` = :height,
-    `birthdate` = :birthdate
-    WHERE id = :id;
+START TRANSACTION;
+    UPDATE kxi_cartitem
+        SET `firstname` = :firstname,
+        `lastname` = :lastname,
+        `serviceid` = :serviceid,
+        `gender` = :gender,
+        `height` = :height,
+        `birthdate` = :birthdate
+        WHERE id = :id;
+COMMIT;
